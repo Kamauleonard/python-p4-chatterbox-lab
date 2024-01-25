ea03bfab3d5f4d970e3ac9f3628e767bb3a030d5
Kamauleonard <leonardmwangi2012@gmail.com>
2024-01-25 18:27:58 +0300

    Initial Commit

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from datetime import datetime
 
 from models import db, Message
 
@@ -8,19 +9,103 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
-
+ 
 CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            jsonify(messages),
+            200
+        )
+
+        return response
+
+    elif request.method == 'POST':
+        data = request.get_json()
+        new_message = Message(
+            body=data.get("body"),
+            username=data.get("username"),
+            created_at=datetime.utcnow()
+        )
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            jsonify(message_dict),
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if  message == None:
+        response_body = {
+            "message": "This record does not exist in our database. Please try again."
+        }
+        response = make_response(response_body, 404)
+
+        return response
+    else:
+        if request.method == 'GET':
+            message_dict = message.to_dict()
+
+            response = make_response(
+                message_dict,
+                200
+            )
+
+            return response
+    
+        elif request.method == 'PATCH':
+            data = request.get_json()
+
+            if data and "body" in data:
+                message.body = data["body"]
+                db.session.commit()
+
+                message_dict = message.to_dict()
+
+                response = make_response(
+                    jsonify(message_dict),
+                    200
+                )
+
+            return response
+
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "message deleted."
+            }
+
+            response = make_response(
+                response_body,
+                200
+            )
+
+            return response
+        
+@app.route('/')
+def index():
+    return "Welcome to the Chatterbox API"
+
 
 if __name__ == '__main__':
     app.run(port=5555)
